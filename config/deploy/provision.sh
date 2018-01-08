21a905d59d0ece5fcea3dbd4fbd98077e98eeca2
pureland <luoyi2006_09@126.com>
2018-01-08 16:49:59 +0800

    fix bug on ubuntu14

--- config/deploy/provision.sh
+++ config/deploy/provision.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 # below is needed to run ruby and nginx, comment things out if you don't need any of them
-echo_msg "installing ruby and it's dependencies"
+echo "installing ruby and it's dependencies"
 sudo apt-get install -yfV -qq libgdbm-dev libncurses5-dev bison libffi-dev curl gawk libyaml-dev libsqlite3-dev sqlite3 libmysqlclient-dev
 sudo apt-get install -yfV -qq libcurl4-openssl-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev libffi-dev python-software-properties
+sudo apt-get install ruby-dev
 
 # install mysql manually:
 # sudo apt-get -q -y install mysql-server
 # mysql -u root -e "create database graphene_prod"
 
-export USER_NAME="ubuntu"
-export USER_HOME="/home/$USER_NAME"
+export USER_NAME="root"
+export USER_HOME="/$USER_NAME"
 export DEFAULT_RUBY="2.2.3"
 
 git clone https://github.com/sstephenson/rbenv.git $USER_HOME/.rbenv
@@ -25,7 +26,7 @@ $USER_HOME/.rbenv/bin/rbenv global $DEFAULT_RUBY
 mkdir $USER_HOME/.rbenv/plugins
 git clone git://github.com/dcarley/rbenv-sudo.git ~/.rbenv/plugins/rbenv-sudo
 
-echo_msg "installing passenger and nginx"
+echo "installing passenger and nginx"
 $USER_HOME/.rbenv/bin/rbenv exec gem install bundler rails passenger
 $USER_HOME/.rbenv/bin/rbenv sudo passenger-install-nginx-module --auto > /dev/null
 
@@ -38,9 +39,9 @@ sudo update-rc.d -f nginx defaults
 
 #sudo mkdir -p /www/graphene
 sudo mkdir -p /www/logs
-sudo chown -R ubuntu:users /www
+sudo chown -R $USER_NAME:users /www
 
 sudo mkdir -p /etc/ssl/certs
 sudo mkdir -p /etc/ssl/private
 
-echo_msg "done"
+echo "done"
